c821d7578b69299d68b8244ad8bcc49653e402ba
sohrabaz <sohi.43348@gmail.com>
2017-07-23 08:00:21 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -90,7 +90,7 @@ function create_config( )
 	"lock-fosh"
 	
  },
-    sudo_users = {123456789},
+    sudo_users = {234545905},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
